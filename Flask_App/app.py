ab879a1f846bbb2d37e2b1683147e0f0e93de2f6
Pradeep A V A <96570568+kxkxshi@users.noreply.github.com>
2026-04-14 01:18:15 +0530

    Update app.run to use environment PORT variable

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -153,5 +153,6 @@ def market():
 # RUN APP
 # =========================
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 

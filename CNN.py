699b2ab554c155ec27bbeea07f762ede4a0620e9
Pradeep A V A <96570568+kxkxshi@users.noreply.github.com>
2026-04-16 21:49:07 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -13,7 +13,7 @@ class CNN(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=32,
                       kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(32), 
             nn.MaxPool2d(2),
             # conv2
             nn.Conv2d(in_channels=32, out_channels=64,
